5f88488bac86aa4b0a29b6b13dec7507432d4eb6
@LuisPerezSM <99104573+LuisPerezSM@users.noreply.github.com>
2022-03-07 14:51:26 -0600

    Subida de la pagina

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+var nav = document.getElementById("nav"),
+  menu = document.getElementById("menu");
+
+menu.onclick = function () {
+  nav.classList.toggle("o-nav");
+  if (nav.classList.contains("o-nav")) {
+    menu.setAttribute("class", "bi bi-x");
+    menu.setAttribute("style", "color: darksalmon; transform: scale(1.3);");
+  } else {
+    menu.setAttribute("class", "bi bi-list");
+    menu.setAttribute("style", "");
+  }
+};
